eefef1fd51b8fbf99d05c58ed289e4b410d2ac18
hugocoutinho <hugocoutinho@brq.com>
2020-06-27 14:21:31 -0300

    readme Files

--- app/src/test/java/com/example/kotlinrepositories/home/domain/repository/MockHomeRepository.kt
+++ app/src/test/java/com/example/kotlinrepositories/home/domain/repository/MockHomeRepository.kt
@@ -24,6 +24,6 @@ object MockHomeRepository {
 
     private fun mockEntity(): HomeRepositoryEntity {
         val owner = Owner("android","https://avatars3.githubusercontent.com/u/32689599?v=4")
-        return HomeRepositoryEntity("architecture-samples",false, owner.login, "A collection of samples to discuss and showcase different architectural tools and patterns for Android apps.","https://github.com/google/flexbox-webview_repository_page","" , 36642,10175)
+        return HomeRepositoryEntity("architecture-samples", owner.login, "A collection of samples to discuss and showcase different architectural tools and patterns for Android apps.","https://github.com/google/flexbox-webview_repository_page","" , 36642,10175)
     }
 }
